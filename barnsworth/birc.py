4ce7c1662d35fed48c5ee1db531f7d2d0c79be3f
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-12-14 15:10:56 -0800

    add wikimon-based parsing of IRC messages for actually-useful websocket content

--- barnsworth/birc.py
+++ barnsworth/birc.py
@@ -1,9 +1,19 @@
 
+import json
+import sys
+sys.path.insert(0, '../../wikimon')  # or pip install wikimon, maybe
+
+
 from geventwebsocket import (WebSocketServer,
                              WebSocketApplication,
                              Resource)
-
 from geventirc import Client, message
+
+from wikimon.parsers import parse_irc_message
+
+# TODO: extract/reconstruct upload URL
+
+server = None
 
 irc = Client('irc.wikimedia.org', 'mahmoud', port=6667)
 
@@ -14,20 +24,23 @@ def print_handler(client, msg):
 
 
 def pub_handler(client, msg):
-    import json
-    json_msg = json.dumps({'length': len(msg.encode())})
+    if msg.command != 'PRIVMSG':
+        print msg.command
+        return
+    msg_content = ' '.join(msg.params[1:]).decode('utf-8')
+    try:
+        msg_dict = parse_irc_message(msg_content)
+    except Exception as e:
+        # log
+        return
+    json_msg = json.dumps(msg_dict)
     for addr, ws_client in server.clients.items():
         ws_client.ws.send(json_msg)
+    return
 
 
 def join_handler(client, msg):
     client.send_message(message.Join('#en.wikipedia'))
-
-
-irc.start()
-irc.add_handler(print_handler)
-irc.add_handler(join_handler, '001')
-irc.add_handler(pub_handler)
 
 
 class EchoApplication(WebSocketApplication):
@@ -38,7 +51,18 @@ class EchoApplication(WebSocketApplication):
         print reason
 
 
-server = WebSocketServer(('', 9000),
-                         Resource({'/': EchoApplication}))
+def main():
+    global server  # todo
+    irc.start()
+    irc.add_handler(print_handler)
+    irc.add_handler(join_handler, '001')
+    irc.add_handler(pub_handler)
+    server = WebSocketServer(('', 9000),
+                             Resource({'/': EchoApplication}))
 
-server.serve_forever()
+    server.serve_forever()
+
+
+
+if __name__ == '__main__':
+    main()

a3a87a4c6d938a18654d1311003915a27ad5bca6
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-12-14 16:26:07 -0800

    packaging up Barnsworth in a class, cleaning up some stuff

--- barnsworth/birc.py
+++ barnsworth/birc.py
@@ -7,40 +7,64 @@ sys.path.insert(0, '../../wikimon')  # or pip install wikimon, maybe
 from geventwebsocket import (WebSocketServer,
                              WebSocketApplication,
                              Resource)
-from geventirc import Client, message
+from geventirc import Client as IRCClient
+from geventirc.message import Join
 
 from wikimon.parsers import parse_irc_message
 
 # TODO: extract/reconstruct upload URL
+# TODO: gevent websockets spinning too hard when there are no clients?
+# TODO: handle nick already in use
+# TODO: hashtags
 
 server = None
 
-irc = Client('irc.wikimedia.org', 'mahmoud', port=6667)
+DEFAULT_IRC_NICK = 'barnsworth'
+DEFAULT_IRC_SERVER = 'irc.wikimedia.org'
+DEFAULT_IRC_PORT = 6667
+DEFAULT_IRC_CHANNELS = ['en.wikipedia']
 
 
-def print_handler(client, msg):
-    #print len(msg.encode())
-    pass
+_JOIN_CODE = '001'
 
 
-def pub_handler(client, msg):
-    if msg.command != 'PRIVMSG':
-        print msg.command
+class Barnsworth(object):
+    def __init__(self, **kwargs):
+        self.irc_nick = kwargs.pop('irc_nick', DEFAULT_IRC_NICK)
+        self.irc_server = kwargs.pop('irc_server', DEFAULT_IRC_SERVER)
+        self.irc_port = kwargs.pop('irc_port', DEFAULT_IRC_PORT)
+        self.irc_channels = [x.strip('#') for x in
+                             kwargs.pop('irc_channels', DEFAULT_IRC_CHANNELS)]
+        # TODO: validate channel formatting?
+        self.irc_client = IRCClient(self.irc_server,
+                                    self.irc_nick,
+                                    self.irc_port)
+        self.irc_client.add_handler(self.join_handler, _JOIN_CODE)
+        self.irc_client.add_handler(self.pub_handler, 'PRIVMSG')
+
+        defer_start = kwargs.pop('defer_start', False)
+        if not defer_start:
+            self._start_irc()
+
+    def join_handler(self, client, msg):
+        # TODO: need another handler to register a join failure?
+        for channel in self.irc_channels:
+            client.send_message(Join(channel))
+
+    def pub_handler(self, client, msg):
+        msg_content = ' '.join(msg.params[1:]).decode('utf-8')
+        try:
+            msg_dict = parse_irc_message(msg_content)
+        except Exception as e:
+            # log
+            return
+        json_msg = json.dumps(msg_dict)
+        for addr, ws_client in server.clients.items():
+            ws_client.ws.send(json_msg)
         return
-    msg_content = ' '.join(msg.params[1:]).decode('utf-8')
-    try:
-        msg_dict = parse_irc_message(msg_content)
-    except Exception as e:
-        # log
-        return
-    json_msg = json.dumps(msg_dict)
-    for addr, ws_client in server.clients.items():
-        ws_client.ws.send(json_msg)
-    return
 
-
-def join_handler(client, msg):
-    client.send_message(message.Join('#en.wikipedia'))
+    def _start_irc(self):
+        self.irc_client.start()
 
 
 class EchoApplication(WebSocketApplication):
@@ -53,15 +77,10 @@ class EchoApplication(WebSocketApplication):
 
 def main():
     global server  # todo
-    irc.start()
-    irc.add_handler(print_handler)
-    irc.add_handler(join_handler, '001')
-    irc.add_handler(pub_handler)
     server = WebSocketServer(('', 9000),
                              Resource({'/': EchoApplication}))
-
+    barnsworth = Barnsworth()
     server.serve_forever()
-
 
 
 if __name__ == '__main__':

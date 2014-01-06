b00c778d9df016aa4123940642b8b288f4fa355c
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2014-01-05 17:01:53 -0800

    add an initial logging library

---
+++ barnsworth/logger.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from logger import BaseLogger
+from filters import ThresholdFilter
+from emitters import StreamEmitter
+from formatters import Formatter
+from sinks import SensibleSink, QuantileSink
+
+# idea here is that any codified lessons learned here will be merged
+# into lithoxyl as one of the sane/sensible defaults.
+
+
+class BarnsworthLogger(BaseLogger):
+    def __init__(self, name, **kwargs):
+        enable_begin = kwargs.pop('enable_begin', True)
+        if kwargs:
+            raise TypeError('unexpected keyword arguments: %r' % kwargs)
+        exc_filter = ThresholdFilter(exception=0)
+        exc_formatter = Formatter('!! {exc_type}: {exc_tb_str}')
+        exc_emitter = StreamEmitter('stderr')
+        exc_sink = SensibleSink(exc_formatter, exc_emitter, [exc_filter])
+
+        #out_filter = ThresholdFilter()
+        # TODO: warn_char (requires len on FormatField)
+        out_formatter = Formatter('{status_char} {logger_name}'
+                                  ' {message} {duration_msecs}')
+        out_emitter = StreamEmitter('stdout')
+        out_sink = SensibleSink(out_formatter, out_emitter)
+        sinks = [QuantileSink(), exc_sink, out_sink]
+        if enable_begin:
+            beg_filter = ThresholdFilter(begin=0)
+            beg_formatter = Formatter('{status_char} {logger_name} {message}')
+            beg_sink = SensibleSink(beg_formatter,
+                                    out_emitter,
+                                    filters=[beg_filter],
+                                    on='begin')
+            sinks.append(beg_sink)
+        super(BarnsworthLogger, self).__init__(name, sinks)
+
+
+if __name__ == '__main__':
+    bl = BarnsworthLogger('test_logger')
+
+    for i in range(5):
+        with bl.debug('test_record_%s' % i) as t:
+            pass
+
+    with bl.info('test_exception', reraise=False) as te:
+        raise ValueError('hm')
